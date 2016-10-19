b62fe3d74754802e5795944b2a582c3c0d29bb3f
emmaga <emma_0812@msn.cn>
2016-10-19 14:43:47 +0800

    添加预定、会员界面

--- src/modules/controllers.js
+++ src/modules/controllers.js
@@ -3,31 +3,72 @@
 (function() {
   var app = angular.module('app.controllers', ['ngCookies'])
   
-  .controller('RootController', ['$scope', '$window', '$http', '$ionicModal', 'backendUrl',
-    function($scope, $window, $http, $ionicModal, backendUrl) {
+  .controller('RootController', ['$scope', '$window', '$http', '$filter', '$ionicModal', 'backendUrl', 
+    function($scope, $window, $http, $filter, $ionicModal, backendUrl) {
       var self = this;
+      
+      // root全局变量：
+      self.params = {};
+      // self.params.appid
+
 
       self.init = function() {
-        // http://openvod.cleartv.cn/index.html?auth_code=xxx&expires_in=600
+
+        // 获取 code 和 appid
         var qString = $window.location.search.substring(1);
         var qParts = qString.parseQuerystring();
-        var authCode = qParts.auth_code;
-        self.getWxAccessToken(authCode);
+        var code = qParts.code;
+
+        // 将 appid 记在 root params 缓存里
+        self.setParams('appid', qParts.appid);
+
+        // wx注册
+        self.wxConfigJSSDK();
+
       }
 
-      self.getWxAccessToken = function(authCode) {
+      self.setParams = function(name, val) {
+        self.params[name] = val;
+      }
 
-        var data = {"auth_code": authCode};
-        console.log(data);
+      self.getParams = function(name) {
+        return self.params[name];
+      }
+
+      self.wxConfigJSSDK = function() {
+
+        // http://www.cnblogs.com/sunshq/p/4171490.html
+        self.noncestr = Math.random().toString(36).substr(2);
+        self.timestamp = new Date().getTime() + '';
+
+        var data = {
+          "appid": self.getParams('appid'),
+          "noncestr": self.noncestr,
+          "timestamp": self.timestamp,
+          "url": window.location.origin + window.location.pathname + window.location.search
+        };
         data = JSON.stringify(data);
-        console.log(data);
 
-        $http.post(backendUrl('page_token', 'server'), data)
+        $http.post(backendUrl('jssign', 'server'), data)
           .success(function(data, status, headers, config) {
-             console.log(data)
+            if(data.rescode == '200') {
+              wx.config({
+                debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                appId: self.getParams('appid'), // 必填，公众号的唯一标识
+                timestamp: self.timestamp, // 必填，生成签名的时间戳
+                nonceStr: self.noncestr, // 必填，生成签名的随机串
+                signature: data.signature,// 必填，签名，见附录1
+                jsApiList: ['getLocation'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+              });
+            }
+            else {
+              // todo use ionic alert style
+              alert($filter('translate')('serverError') + data.rescode);
+            }
           })
           .error(function(data, status, headers, config) {
-             alert(status)
+            // todo use ionic alert style
+            alert($filter('translate')('serverError') + status);
           })
       }
 
@@ -42,24 +83,6 @@
          $scope.modal.show();
       }
   }])
-
-  .controller('bookHotelSearchController', ['$scope', '$location', '$http', 
-    function($scope, $location, $http) {
-
-      var self = this;
-
-      self.init = function() {
-        // 显示／隐藏日期选择器参数初始化
-        self.datePickerShow = false;
-      };
-
-      // 显示／隐藏日期选择器
-      self.showDP = function(boo) {
-        self.datePickerShow = boo ? boo : false;
-      };
-
-    }
-  ])
 
   .controller('bookHotelListController', ['$scope', '$location', '$http', '$stateParams', 'backendUrl',
     function($scope, $location, $http, $stateParams, backendUrl) {
@@ -104,23 +127,112 @@
     }
   ])
 
-  .controller('userOrderController', ['$scope', '$ionicModal',
-    function($scope, $ionicModal) {
+  .controller('bookRoomListController', ['$http', 
+    function($http) {
+      var self = this;
+
+      self.init = function() {
+        
+      }
+    }
+  ])
+
+  .controller('hotelInfoController', ['$http', 
+    function($http) {
+      var self = this;
+      
+      self.init = function() {
+        
+      }
+    }
+  ])
+
+  .controller('bookInfoController', ['$http', 
+    function($http) {
+      var self = this;
+      
+      self.init = function() {
+        
+      }
+    }
+  ])
+
+  .controller('bookResultController', ['$http', 
+    function($http) {
+      var self = this;
+      
+      self.init = function() {
+        
+      }
+    }
+  ])
+
+  .controller('bookRoomSoldOutController', ['$http', 
+    function($http) {
+      var self = this;
+      
+      self.init = function() {
+        
+      }
+    }
+  ])
+  
+  .controller('memberHomeController', ['$http', 
+    function($http) {
+      var self = this;
+      
+      self.init = function() {
+        
+      }
+    }
+  ])
+
+  .controller('memberLoginController', ['$http', 
+    function($http) {
+      var self = this;
+      
+      self.init = function() {
+        
+      }
+    }
+  ])
+
+  .controller('memberRegisterController', ['$http', 
+    function($http) {
+      var self = this;
+      
+      self.init = function() {
+        
+      }
+    }
+  ])
+
+  .controller('memberInfoEditController', ['$http', 
+    function($http) {
+      var self = this;
+      
+      self.init = function() {
+        
+      }
+    }
+  ])
+
+  .controller('memberResetPWController', ['$http', 
+    function($http) {
+      var self = this;
+      
+      self.init = function() {
+        
+      }
+    }
+  ])
+
+  .controller('memberOrderListController', ['$scope',
+    function($scope) {
       var self = this;
 
       self.init = function() {
       }
-      // menu modal 弹出
-      $ionicModal.fromTemplateUrl('pages/mainMenu.html', {
-        scope: $scope,
-        animation: 'slide-in-left'
-      }).then(function(modal) {
-        $scope.modal = modal;
-      });
-      self.showMainMenu = function() {
-         $scope.modal.show();
-      }
   }])
-  
 
 })();
