7213326d059ac5ceae10bd5bfa3244099b63f9b9
emmaga <emma_0812@msn.cn>
2016-11-03 14:06:46 +0800

    wx pay 调试

--- src/modules/controllers.js
+++ src/modules/controllers.js
@@ -680,7 +680,7 @@
         $http.post(backendUrl('neworder', '', 'server'), data)
           .success(function(data, status, headers, config) {
             if(data.rescode == '200') {
-              self.wxPay(data.nonce_str, data.sign, data.prepay_id, data.orderNum);
+              self.wxPay(data.JS_Pay_API, data.orderNum);
             }
             else {
               // todo use ionic alert style
@@ -693,18 +693,25 @@
           })
       }
 
-      self.wxPay = function(nonceStr, paySign, prepay_id, orderId) {
+      self.wxPay = function(JS_Pay_API, orderId) {
         var orderId = orderId;
+        console.log(JS_Pay_API.timeStamp + ', ' + JS_Pay_API.nonceStr + ', ' + JS_Pay_API.package + ', ' + JS_Pay_API.signType + ', ' + JS_Pay_API.paySign);
         wx.chooseWXPay({
-          timeStamp: Math.floor(new Date().getTime()/1000), // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-          nonceStr: nonceStr, // 支付签名随机串，不长于 32 位
-          package: 'prepay_id='+prepay_id, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-          signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-          paySign: paySign, // 支付签名
+          timestamp: JS_Pay_API.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+          nonceStr: JS_Pay_API.nonceStr, // 支付签名随机串，不长于 32 位
+          package: JS_Pay_API.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+          signType: JS_Pay_API.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+          paySign: JS_Pay_API.paySign, // 支付签名
           success: function (res) {
               // 支付成功后的回调函数
               $state.go('orderInfo', {orderId:orderId})
-          }
+          },
+            cancel: function () {
+                $state.go('orderInfo', {orderId:orderId})
+            },
+            error: function (e) {
+                $state.go('orderInfo', {orderId:orderId})
+            }
         });
       }
     }
@@ -728,7 +735,6 @@
         self.search();
       }
       self.search = function() {
-
         var data = {
           "action": "GetRoomOrderDetail",
           "appid": $scope.root.getParams('appid'),
