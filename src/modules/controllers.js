565900ce965d24ea905cee07f32ebb9ed9e79b00
emmaga <emma_0812@msn.cn>
2016-11-17 15:41:41 +0800

    shoporderinfo pay

--- src/modules/controllers.js
+++ src/modules/controllers.js
@@ -1866,6 +1866,67 @@
                 }, 500)
 
             }
+
+            self.pay = function() {
+                // 等待支付
+                // 支付按钮变为不可点击，防止多次点击
+                document.getElementById('payBtn').disabled = true;
+
+
+                //获取支付参数
+                $ionicLoading.show({
+                   template: '等待支付...'
+                });
+                var data = {
+                    "clear_session": $scope.root.getParams('clear_session'),
+                    "appid": $scope.root.getParams('appid'),
+                    "openid": $scope.root.getParams('wxUserInfo')&&$scope.root.getParams('wxUserInfo').openid,
+                    "lang": $translate.proposedLanguage() || $translate.use(),
+                    "hotelId": self.hotelId,
+                    "orderID": self.orderId,
+                    "payType": "JSAPI"
+                }
+                data = JSON.stringify(data);
+
+                $http({
+                  method: $filter('ajaxMethod')(),
+                  url: backendUrl('shoporder', 'memberAddress'),
+                  data: data
+                })
+                .then(function successCallback(data, status, headers, config) {
+                  if(data.data.rescode == '200') {
+                    var wxP = data.data.data.JS_Pay_API;
+                    wx.chooseWXPay({
+                        timestamp: wxP.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+                        nonceStr: wxP.nonceStr, // 支付签名随机串，不长于 32 位
+                        package: wxP.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+                        signType: wxP.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+                        paySign: wxP.paySign, // 支付签名
+                        success: function(res) {
+                            // 支付后跳转到订单详情页面
+                            $state.reload();
+                        },
+                        cancel: function() {
+                            $state.reload();
+                        },
+                        error: function(e) {
+                            $state.reload();
+                        }
+                    });
+                  }
+                  else {
+                      alert($filter('translate')('serverError') + data.data.rescode + data.data.errInfo);
+                  }
+                })
+                .finally(function(value){
+                  $ionicLoading.hide();
+                  document.getElementById('payBtn').disabled = false;
+                });
+            }
+
+            self.canelOrder = function() {
+                
+            }
         }
     ])
     
