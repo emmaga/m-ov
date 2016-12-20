df9e8d761a63c26a0aaf57c8b28a3efc95374483
emmaga <emma_0812@msn.cn>
2016-12-20 14:53:35 +0800

    禁用 "分享到QQ","分享到QQ空间"

--- src/modules/controllers.js
+++ src/modules/controllers.js
@@ -154,14 +154,16 @@
                         });
                         wx.ready(function(){
                             // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-                            // 禁用“分享给好友”，“分享到朋友圈”, "在Safari中打开", "邮件","复制链接"
+                            // 禁用“分享给好友”，“分享到朋友圈”, "在Safari中打开", "邮件","复制链接","分享到QQ","分享到QQ空间"
                             wx.hideMenuItems({
                                 menuList: [
                                     'menuItem:share:appMessage', 
                                     'menuItem:share:timeline', 
                                     'menuItem:openWithSafari',
                                     'menuItem:share:email', 
-                                    'menuItem:copyUrl'
+                                    'menuItem:copyUrl',
+                                    'menuItem:share:qq',
+                                    'menuItem:share:QZone'
                                 ]
                             });
                         });
