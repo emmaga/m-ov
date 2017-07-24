87586ea6081c7e079e510106e99b0028f8400084
Emma <emma_0812@msn.cn>
2017-07-24 14:05:45 +0800

    将sessionstorage 改回cookie存储

--- src/modules/services.js
+++ src/modules/services.js
@@ -99,8 +99,7 @@
        * 设置变量
        */
       'setParams': function (paramsName, value) {
-          // $cookies.put(paramsName, JSON.stringify(value))
-          sessionStorage && sessionStorage.setItem(paramsName, JSON.stringify(value));
+          $cookies.put(paramsName, JSON.stringify(value))
       },
       /**
        * 获取随机数
@@ -121,16 +120,11 @@
        * @returns {*}
        */
       'getParams': function (paramsName) {
-          // if($cookies.get(paramsName)) {
-          //     return JSON.parse($cookies.get(paramsName));
-          // }
-          // else {
-          //     return false;
-          // }
-          if(sessionStorage && sessionStorage.getItem(paramsName)) {
-            return JSON.parse(sessionStorage.getItem(paramsName));
-          } else {
-            return false;
+          if($cookies.get(paramsName)) {
+              return JSON.parse($cookies.get(paramsName));
+          }
+          else {
+              return false;
           }
       }
       // 变量：
