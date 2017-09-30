e6254905d43189054c736279b7cea8d6ad0f8e36
Emma <emma_0812@msn.cn>
2017-09-30 14:19:44 +0800

    修复安卓转发有问题的问题

--- src/modules/controllers.js
+++ src/modules/controllers.js
@@ -3831,32 +3831,34 @@
                     var uid = util.getParams('userid')
                     var uaid = util.getSearchParams('uaid') == null ? -1 : util.getSearchParams('uaid')
                     var shareLink = BACKEND_CONFIG.serverUrl+'fxredirect?ht_appid='+appid+'&mch_appid='+mch_appid+'&puid='+uid+'&puaid='+uaid+'&uid=-1&uaid=-1&sid='+sid+'&gid='+gid
-                    wx.onMenuShareTimeline({
-                        title: self.productInfo.title, // 分享标题
-                        link: shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                        desc: self.productInfo.shareDesc,
-                        imgUrl: self.productInfo.img, // 分享图标
-                        success: function () { 
-                            // 用户确认分享后执行的回调函数
-                        },
-                        cancel: function () { 
-                            // 用户取消分享后执行的回调函数
-                        }
-                    });
-                    wx.onMenuShareAppMessage({
-                        title: self.productInfo.title, // 分享标题
-                        link: shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                        desc: self.productInfo.shareDesc,
-                        imgUrl: self.productInfo.img, // 分享图标
-                        type: '', // 分享类型,music、video或link，不填默认为link
-                        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-                        success: function () { 
-                            // 用户确认分享后执行的回调函数
-                        },
-                        cancel: function () { 
-                            // 用户取消分享后执行的回调函数
-                        }
-                    });
+                    wx.ready(function () {
+                        wx.onMenuShareTimeline({
+                            title: self.productInfo.title, // 分享标题
+                            link: shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                            desc: self.productInfo.shareDesc,
+                            imgUrl: self.productInfo.img, // 分享图标
+                            success: function () { 
+                                // 用户确认分享后执行的回调函数
+                            },
+                            cancel: function () { 
+                                // 用户取消分享后执行的回调函数
+                            }
+                        });
+                        wx.onMenuShareAppMessage({
+                            title: self.productInfo.title, // 分享标题
+                            link: shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                            desc: self.productInfo.shareDesc,
+                            imgUrl: self.productInfo.img, // 分享图标
+                            type: '', // 分享类型,music、video或link，不填默认为link
+                            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                            success: function () { 
+                                // 用户确认分享后执行的回调函数
+                            },
+                            cancel: function () { 
+                                // 用户取消分享后执行的回调函数
+                            }
+                        });
+                    })
                 }
 
                 self.getProductInfo = function () {
